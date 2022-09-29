544e0b371216483a051416b522141be5eac25368
Vkakvey <casimeroball14@gmail.com>
2022-09-29 15:56:39 +0700

    day-3

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -4,8 +4,26 @@ import DefaultLayout from "../../layout/DefaultLayout";
 import Nav from "../../nav";
 const App:React.FC = () => {
   return (
+    
     <DefaultLayout>
+    {/* Nav */}
      <Nav/>
+    {/* End of nav */}
+
+    {/* Hero */}
+    {/* End of hero */}
+
+    {/* About */}
+    {/* End of about */}
+
+    {/* Menu */}
+    {/* End of menu */}
+
+    {/* Contact */}
+    {/* End of Contact */}
+
+    {/* Footer */}
+    {/* End of footer */}
     </DefaultLayout> 
   )
 } 
