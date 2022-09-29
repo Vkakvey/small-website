e8ff4ae7af5ecb979162b4837430b66c69d3f819
Vkakvey <casimeroball14@gmail.com>
2022-09-29 16:23:25 +0700

    day-3

--- src/nav/index.tsx
+++ src/nav/index.tsx
@@ -1,21 +1,46 @@
 import React from "react";
 import "./index.css";
 interface Navprops{}
+const menubar = [
+    {
+      link:"#about",
+      label:"About",
+    },
+    {
+        link:"#menu",
+        label:"Menu",
+    },
+    {
+        link:"#contact",
+        label:"Contact",
+    }
+
+];
 const Nav: React.FC<Navprops> = (props) => {
     const {} = props;
+    console.log("HELLO MENU BAR: ", menubar);
+    
     return (
         <nav>
             <a href="#" className="logo">Food Website</a>
+            {/* <ul>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#menu">Menu</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+            </ul> */}
+
             <ul>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-            <li>
-                <a href="#">Menu</a>
-            </li>
+                {menubar.map((items, index) => (
+                    <li key={index}>
+                        <a href={items.link}>{items.label}</a>
+                    </li>
+                  ))}
             </ul>
         </nav>
     )
